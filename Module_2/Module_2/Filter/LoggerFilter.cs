ce32300bb2e07b8fcd106c59d7ebafdc8ccd782b
Ruslan <borodaj.ruslan718@gmail.com>
2021-05-21 14:02:20 +0300

    module

---
+++ Module_2/Module_2/Filter/LoggerFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module_2.Filter
+{
+    public class LoggerFilter : ActionFilterAttribute
+    {
+        private readonly ILogger _logger;
+        public LoggerFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("LoggerFilter");
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogWarning("Inside OnActionExecuting method...");
+            base.OnActionExecuting(context);
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogWarning("Inside OnActionExecuted method...");
+            base.OnActionExecuted(context);
+        }
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            _logger.LogWarning("Inside OnResultExecuting method...");
+            base.OnResultExecuting(context);
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            _logger.LogWarning("Inside OnResultExecuted method...");
+            base.OnResultExecuted(context);
+        }
+    }
+}
